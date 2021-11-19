a2966295f87357f435002ecccc933274c2f0a430
Tomás Vicente <up201904609@fe.up.pt>
2021-11-19 21:25:24 +0000

    Abort application layer on error.

--- proj/src/application_layer/emitter.c
+++ proj/src/application_layer/emitter.c
@@ -14,7 +14,6 @@
 #define BUFFER_SIZE 2048
 
 int main(int argc, char **argv) {
-    unsigned char no = 0;
     if (argc < 3 || argc > 4) {
         fprintf(stderr, RED"Usage:\temitter SerialPort FilePath [FileName]\n\tex: emitter /dev/ttyS1\n"RESET);
         exit(1);
@@ -23,10 +22,14 @@ int main(int argc, char **argv) {
     printf(YELLOW"[emitter]: started: using serial port: %s\n"RESET, argv[1]);
 
     int fd2 = open(argv[2], O_RDONLY);
-    if (fd2 < 0) {}
+    if (fd2 < 0) {
+        exit(-1);
+    }
 
     int fd = ll_open(argv[1], true);
-    if (fd < 0) exit(-1);
+    if (fd < 0) {
+        exit(-1);
+    }
 
     control_packet_t packet;
     packet.file_size = get_file_size(argv[2]);
@@ -38,26 +41,46 @@ int main(int argc, char **argv) {
 
     {
         n = assemble_control_packet(packet, true, pa, sizeof(pa));
-        if (n < 0) {}
-        if (ll_write(fd, pa, n) < 0) {}
-    }
+        if (n < 0) {
+            exit(-1);
+        }
 
-    {
-        while (true) {
-            n = read(fd2, buf, sizeof(buf));
-            if (n <= 0) break;
-            n = assemble_data_packet(buf, n, pa, sizeof(pa), no % 256);
-            if (n < 0) {}
-            if (ll_write(fd, pa, n) < 0) {}
-            ++no;
+        if (ll_write(fd, pa, n) < 0) {
+            exit(-1);
         }
     }
 
-    {
-        n = assemble_control_packet(packet, false, pa, sizeof(pa));
-        if (n < 0) {}
-        if (ll_write(fd, pa, n) < 0) {}
-        if (ll_close(fd, true) < 0) exit(-1);
+    size_t no_packets = packet.file_size / BUFFER_SIZE;
+    if (no_packets * BUFFER_SIZE < packet.file_size) ++no_packets;
+
+    for (size_t i = 0; i < no_packets; ++i) {
+        n = read(fd2, buf, sizeof(buf));
+        if (n <= 0) {
+            exit(-1);
+        }
+
+        n = assemble_data_packet(buf, n, pa, sizeof(pa), i % 256);
+        if (n < 0) {
+            exit(-1);
+        }
+
+        if (ll_write(fd, pa, n) < 0) {
+            exit(-1);
+        }
     }
+
+    n = assemble_control_packet(packet, false, pa, sizeof(pa));
+    if (n < 0) {
+        exit(-1);
+    }
+
+    if (ll_write(fd, pa, n) < 0) {
+        exit(-1);
+    }
+
+    if (ll_close(fd, true) < 0) {
+        exit(-1);
+    }
+
     return 0;
 }
--- proj/src/application_layer/receiver.c
+++ proj/src/application_layer/receiver.c
@@ -24,7 +24,6 @@ int main(int argc, char **argv) {
         exit(-1);
     }
 
-
     ssize_t r;
     control_packet_t start_packet, end_packet;
     memset(&start_packet, 0, sizeof(start_packet));
@@ -32,61 +31,64 @@ int main(int argc, char **argv) {
 
     unsigned char packet[PACKET_SIZE];
     r = ll_read(fd, packet, sizeof(packet));
+    if (r < 0) {
+        exit(-1);
+    }
 
-
-    if (r == EOF_DISCONNECT) {
-    } else if (r < 0) {
-    } else if (r >= 0) {
-        if (process_control_packet(packet, r, &start_packet, true) < 0) {
-        }
-    } else {
-        // Does it make sense for any packet to arrive before this one?
+    if (process_control_packet(packet, r, &start_packet, true) < 0) {
+        exit(-1);
     }
 
     int fd2 = open(start_packet.file_name, O_WRONLY | O_CREAT, 0644);
-    if (fd2 < 0) {}
+    if (fd2 < 0) {
+        exit(-1);
+    }
 
     size_t no_packets = start_packet.file_size / PACKET_SIZE + 1;
 
     size_t i = 0;
     size_t n;
 
-    unsigned char buf[BUFFER_SIZE];
+    unsigned char data[BUFFER_SIZE];
     while (true) {
         r = ll_read(fd, packet, sizeof(packet));
         if (r == EOF_DISCONNECT) {
             break;
         } else if (r < 0) {
-            // Raise an error
+            exit(-1);
         } else if (r >= 0) {
             if (packet[0] == C_DATA) {
-                n = process_data_packet(packet, r, buf, i % 256, sizeof(buf));
+                n = process_data_packet(packet, r, data, i % 256, sizeof(data));
                 if (n < 0) {
-                    continue;
+                    exit(-1);
                 } else {
-                    printf("WRITING: %s\n", buf);
-                    if (write(fd2, buf, n) < 0) {}
+                    if (write(fd2, data, n) < 0) {
+                        exit(-1);
+                    }
                 }
-
             } else {
                 if (process_control_packet(packet, r, &end_packet, false) < 0) {
-                    // Raise an error
                 } else {
-                    break;
+                    exit(-1);
                 }
-                // From this point on, does it make sense for other packets to arrive?
             }
             ++i;
         }
     }
 
-    if (start_packet.file_size != end_packet.file_size || strcmp(start_packet.file_name, end_packet.file_name) != 0) {
+    if (start_packet.file_size != end_packet.file_size ||
+        (start_packet.file_name != NULL && end_packet.file_name != NULL &&
+         strcmp(start_packet.file_name, end_packet.file_name) != 0)) {
+        exit(-1);
     }
 
     free(start_packet.file_name);
     free(end_packet.file_name);
 
 
-    if (ll_close(fd, false) < 0) exit(-1);
+    if (ll_close(fd, false) < 0) {
+        exit(-1);
+    }
+
     return SUCCESS;
 }
